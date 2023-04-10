f619901844146c6f4d521a92d612091493147054
user <ashirova158@gmail.com>
2023-04-10 12:22:08 +0600

    base for blog django

---
+++ blog/post/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model() #создали класс Пользователь
+
+
+class Post(models.Model): #models - это файл, где есть класс Model
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') #этот режим говорит,что если удаляется юзер, то удаляется вся таблица
+    # related_name= - 
+    title = models.CharField(max_length=100)  #CharField - тип данных строка, тоже класс
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) #позволяет создавать дату автоматически
+
+
