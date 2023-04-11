b4771c2bffc83114cf03b5d280b4b8868de63c53
user <ashirova158@gmail.com>
2023-04-11 12:03:02 +0600

    post listing with templates

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render #он соединяет 2 файла?
+from .models import Post
 
 # Create your views here.
+
+def posts_list(request):
+    queryset = Post.objects.all()  #objects - в ней содержится все данные в бд
+    return render(request, 'listing.html', {'posts' : queryset})
+    # posts - при обращении по этому ключи мы переходим в queryset
